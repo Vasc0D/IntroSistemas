baa8ce19ff2f17cc5f50703613b91742e334809c
Vasc0D <vasco.diaz@utec.edu.pe>
2026-04-30 17:16:53 -0500

    update: show actual hour

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ function obtenerMensaje() {
   return "¡Hola Mundo desde Node.js en la nube!";
 }
 
+function obtenerHora() {
+  const fecha = new Date();
+  return fecha.toLocaleString("es-ES", {
+    timeZone: "America/Lima",
+    hour12: true,
+  });
+}
+
 app.get("/", (req, res) => {
-  res.send(obtenerMensaje());
+  const mensaje = obtenerMensaje();
+  const hora = obtenerHora();
+  res.send(`
+    <p>${mensaje}</p>
+    <p>Hora actual: ${hora}</p>
+  `);
 });
 
 app.get("/health", (req, res) => res.status(200).send("ok"));
